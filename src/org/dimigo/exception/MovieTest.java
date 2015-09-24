bf7eb0b86953863249de7a6ae926ef2fd492b6a0
ganddamuniya <ganddamuniya@naver.com>
2015-09-24 11:31:45 +0900

    .

--- src/org/dimigo/exception/MovieTest.java
+++ src/org/dimigo/exception/MovieTest.java
@@ -5,20 +5,22 @@ package org.dimigo.exception;
  */
 public class MovieTest {
     public static void main(String[] args) {
-        try {
-            Movie[] movies = {
-                    new Movie("앤트맨", 12), new Movie("사도", 12), new Movie("베테랑", 15)
-            };
-            int age = 13;
-            for(Movie movie : movies) {
+
+        Movie[] movies = {
+                new Movie("베테랑", 15), new Movie("앤트맨", 12), new Movie("사도", 12)
+        };
+        int age = 13;
+        for (Movie movie : movies) {
+            try {
                 buyTicket(movie, age);
+            } catch(Exception e) {
+                System.out.println(e.getMessage());
             }
-        } catch (Exception e) {
-            System.out.println(e.getMessage());
         }
     }
-    private static void buyTicket(Movie movie, int age) throws Exception{
-        if(age >= movie.getLimitAge()) {
+
+    private static void buyTicket(Movie movie, int age) throws Exception {
+        if (age >= movie.getLimitAge()) {
             System.out.println(movie.getTitle() + " 영화 즐감하세요.");
         } else {
             throw new AgeCheckEception(movie);
