4792e9b0cdfbd5789e882d2c9f9d9d4fc1b30fbf
ganddamuniya <ganddamuniya@naver.com>
2015-09-24 12:48:23 +0900

    .

---
+++ src/org/dimigo/collection/Music.java
@@ -0,0 +1,38 @@
+package org.dimigo.collection;
+
+/**
+ * Created by gandd on 2015-09-24.
+ */
+public class Music {
+    private String title;
+    private String singer;
+
+    public Music(String title, String singer) {
+        this.title = title;
+        this.singer = singer;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getSinger() {
+        return singer;
+    }
+
+    public void setSinger(String singer) {
+        this.singer = singer;
+    }
+
+    @Override
+    public String toString() {
+        return "Music [" +
+                "title='" + title +
+                ", singer='" + singer +
+                ']';
+    }
+}
